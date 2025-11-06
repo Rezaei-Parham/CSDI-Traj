db20814d42b5afbcaa0104c2c2c242f4355ab968
Parham <94908814+Rezaei-Parham@users.noreply.github.com>
2025-11-06 08:40:31 +0330

    Update dataset_physio.py

--- dataset_physio.py
+++ dataset_physio.py
@@ -1,177 +1,65 @@
-import pickle
+// demo_profile.c
+// Build (slow build for profiling):
+//   gcc -O0 -g -fno-omit-frame-pointer -o demo demo_profile.c
+// Run:
+//   ./demo             # or ./demo 2 to make it slower
 
-import os
-import re
-import numpy as np
-import pandas as pd
-from torch.utils.data import DataLoader, Dataset
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-# 35 attributes which contains enough non-values
-attributes = ['DiasABP', 'HR', 'Na', 'Lactate', 'NIDiasABP', 'PaO2', 'WBC', 'pH', 'Albumin', 'ALT', 'Glucose', 'SaO2',
-              'Temp', 'AST', 'Bilirubin', 'HCO3', 'BUN', 'RespRate', 'Mg', 'HCT', 'SysABP', 'FiO2', 'K', 'GCS',
-              'Cholesterol', 'NISysABP', 'TroponinT', 'MAP', 'TroponinI', 'PaCO2', 'Platelets', 'Urine', 'NIMAP',
-              'Creatinine', 'ALP']
+static long fib_slow(int n) {          // exponential-time on purpose
+    if (n <= 1) return n;
+    return fib_slow(n-1) + fib_slow(n-2);
+}
 
+static void bubble_sort(int *a, int n) { // O(n^2) on purpose
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n - 1; j++)
+            if (a[j] > a[j+1]) {
+                int t = a[j]; a[j] = a[j+1]; a[j+1] = t;
+            }
+}
 
-def extract_hour(x):
-    h, _ = map(int, x.split(":"))
-    return h
+static char* append_slow(char *s, const char *add) { // realloc each time
+    size_t old = s ? strlen(s) : 0, addl = strlen(add);
+    char *p = realloc(s, old + addl + 1);
+    if (!p) { free(s); return NULL; }
+    memcpy(p + old, add, addl + 1);
+    return p;
+}
 
+int main(int argc, char **argv) {
+    int scale = (argc > 1) ? atoi(argv[1]) : 1;
+    if (scale < 1) scale = 1;
 
-def parse_data(x):
-    # extract the last value for each attribute
-    x = x.set_index("Parameter").to_dict()["Value"]
+    struct timespec t0, t1;
+    clock_gettime(CLOCK_MONOTONIC, &t0);
 
-    values = []
+    // 1) Bubble sort a big array (hotspot #1)
+    int n = 15000 * scale;
+    int *arr = (int*)malloc(n * sizeof(int));
+    if (!arr) { perror("malloc"); return 1; }
+    for (int i = 0; i < n; i++) arr[i] = rand();
+    bubble_sort(arr, n);
 
-    for attr in attributes:
-        if x.__contains__(attr):
-            values.append(x[attr])
-        else:
-            values.append(np.nan)
-    return values
+    // 2) Naive Fibonacci a few times (hotspot #2)
+    long fibsum = 0;
+    for (int i = 0; i < 2 * scale; i++) fibsum += fib_slow(42);
 
+    // 3) Terrible string builder (hotspot #3)
+    char *s = NULL;
+    for (int i = 0; i < 20000 * scale; i++) {
+        s = append_slow(s, "x");
+        if (!s) { fprintf(stderr, "alloc failed\n"); break; }
+    }
 
-def parse_id(id_, missing_ratio=0.1):
-    data = pd.read_csv("./data/physio/set-a/{}.txt".format(id_))
-    # set hour
-    data["Time"] = data["Time"].apply(lambda x: extract_hour(x))
-
-    # create data for 48 hours x 35 attributes
-    observed_values = []
-    for h in range(48):
-        observed_values.append(parse_data(data[data["Time"] == h]))
-    observed_values = np.array(observed_values)
-    observed_masks = ~np.isnan(observed_values)
-
-    # randomly set some percentage as ground-truth
-    masks = observed_masks.reshape(-1).copy()
-    obs_indices = np.where(masks)[0].tolist()
-    miss_indices = np.random.choice(
-        obs_indices, (int)(len(obs_indices) * missing_ratio), replace=False
-    )
-    masks[miss_indices] = False
-    gt_masks = masks.reshape(observed_masks.shape)
-
-    observed_values = np.nan_to_num(observed_values)
-    observed_masks = observed_masks.astype("float32")
-    gt_masks = gt_masks.astype("float32")
-
-    return observed_values, observed_masks, gt_masks
-
-
-def get_idlist():
-    patient_id = []
-    for filename in os.listdir("./data/physio/set-a"):
-        match = re.search("\d{6}", filename)
-        if match:
-            patient_id.append(match.group())
-    patient_id = np.sort(patient_id)
-    return patient_id
-
-
-class Physio_Dataset(Dataset):
-    def __init__(self, eval_length=48, use_index_list=None, missing_ratio=0.0, seed=0):
-        self.eval_length = eval_length
-        np.random.seed(seed)  # seed for ground truth choice
-
-        self.observed_values = []
-        self.observed_masks = []
-        self.gt_masks = []
-        path = (
-            "./data/physio_missing" + str(missing_ratio) + "_seed" + str(seed) + ".pk"
-        )
-
-        if os.path.isfile(path) == False:  # if datasetfile is none, create
-            idlist = get_idlist()
-            for id_ in idlist:
-                try:
-                    observed_values, observed_masks, gt_masks = parse_id(
-                        id_, missing_ratio
-                    )
-                    self.observed_values.append(observed_values)
-                    self.observed_masks.append(observed_masks)
-                    self.gt_masks.append(gt_masks)
-                except Exception as e:
-                    print(id_, e)
-                    continue
-            self.observed_values = np.array(self.observed_values)
-            self.observed_masks = np.array(self.observed_masks)
-            self.gt_masks = np.array(self.gt_masks)
-
-            # calc mean and std and normalize values
-            # (it is the same normalization as Cao et al. (2018) (https://github.com/caow13/BRITS))
-            tmp_values = self.observed_values.reshape(-1, 35)
-            tmp_masks = self.observed_masks.reshape(-1, 35)
-            mean = np.zeros(35)
-            std = np.zeros(35)
-            for k in range(35):
-                c_data = tmp_values[:, k][tmp_masks[:, k] == 1]
-                mean[k] = c_data.mean()
-                std[k] = c_data.std()
-            self.observed_values = (
-                (self.observed_values - mean) / std * self.observed_masks
-            )
-
-            with open(path, "wb") as f:
-                pickle.dump(
-                    [self.observed_values, self.observed_masks, self.gt_masks], f
-                )
-        else:  # load datasetfile
-            with open(path, "rb") as f:
-                self.observed_values, self.observed_masks, self.gt_masks = pickle.load(
-                    f
-                )
-        if use_index_list is None:
-            self.use_index_list = np.arange(len(self.observed_values))
-        else:
-            self.use_index_list = use_index_list
-
-    def __getitem__(self, org_index):
-        index = self.use_index_list[org_index]
-        s = {
-            "observed_data": self.observed_values[index],
-            "observed_mask": self.observed_masks[index],
-            "gt_mask": self.gt_masks[index],
-            "timepoints": np.arange(self.eval_length),
-        }
-        return s
-
-    def __len__(self):
-        return len(self.use_index_list)
-
-
-def get_dataloader(seed=1, nfold=None, batch_size=16, missing_ratio=0.1):
-
-    # only to obtain total length of dataset
-    dataset = Physio_Dataset(missing_ratio=missing_ratio, seed=seed)
-    indlist = np.arange(len(dataset))
-
-    np.random.seed(seed)
-    np.random.shuffle(indlist)
-
-    # 5-fold test
-    start = (int)(nfold * 0.2 * len(dataset))
-    end = (int)((nfold + 1) * 0.2 * len(dataset))
-    test_index = indlist[start:end]
-    remain_index = np.delete(indlist, np.arange(start, end))
-
-    np.random.seed(seed)
-    np.random.shuffle(remain_index)
-    num_train = (int)(len(dataset) * 0.7)
-    train_index = remain_index[:num_train]
-    valid_index = remain_index[num_train:]
-
-    dataset = Physio_Dataset(
-        use_index_list=train_index, missing_ratio=missing_ratio, seed=seed
-    )
-    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=1)
-    valid_dataset = Physio_Dataset(
-        use_index_list=valid_index, missing_ratio=missing_ratio, seed=seed
-    )
-    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=0)
-    test_dataset = Physio_Dataset(
-        use_index_list=test_index, missing_ratio=missing_ratio, seed=seed
-    )
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=0)
-    return train_loader, valid_loader, test_loader
+    free(s);
+    free(arr);
+    clock_gettime(CLOCK_MONOTONIC, &t1);
+    double dt = (t1.tv_sec - t0.tv_sec) + (t1.tv_nsec - t0.tv_nsec)/1e9;
+    printf("Done. scale=%d, time=%.2f s, fibsum=%ld, strlen=%zu\n",
+           scale, dt, fibsum, s ? strlen(s) : 0);
+    return 0;
+}
